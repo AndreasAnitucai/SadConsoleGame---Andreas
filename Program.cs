ae39676d4ce6b403805f2810419f29305ce2686a
h22tiob <h22tiob@axevalla.fhsk.se>
2024-02-21 11:50:09 +0100

    uppgift 4

--- Program.cs
+++ Program.cs
@@ -1,14 +1,58 @@
 ﻿using SadConsole.Configuration;
+using SadConsoleGame;
 
-Settings.WindowTitle = "My SadConsole Game";
+Settings.WindowTitle = "Adv.Programmering Uppgift - AA";
 
-Game.Create(90, 30, Startup);
+Builder configuration = new Builder()
+    .SetScreenSize(120, 38)
+    .SetStartingScreen<RootScreen>()
+    .IsStartingScreenFocused(true)
+    ;
+
+Game.Create(configuration);
 Game.Instance.Run();
 Game.Instance.Dispose();
 
 static void Startup(object? sender, GameHost host)
 {
-    Game.Instance.StartingConsole!.FillWithRandomGarbage(SadConsole.Game.Instance.StartingConsole!.Font);
-    Game.Instance.StartingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, Mirror.None);
-    Game.Instance.StartingConsole.Print(4, 4, "Hello from SadConsole");
+    ScreenObject container = new ScreenObject();
+    Game.Instance.Screen = container;
+
+    // First console
+    Console console1 = new(60, 14);
+    console1.Position = (3, 2);
+    console1.Surface.DefaultBackground = Color.AnsiCyan;
+    console1.Clear();
+    console1.Print(1, 1, "Type on me!");
+    console1.Cursor.Position = (1, 2);
+    console1.Cursor.IsEnabled = true;
+    console1.Cursor.IsVisible = true;
+    console1.Cursor.MouseClickReposition = true;
+    console1.IsFocused = true;
+
+
+    // Add a child surface
+    ScreenSurface surfaceObject = new(5, 3);
+    surfaceObject.Surface.FillWithRandomGarbage(surfaceObject.Font);
+    surfaceObject.Position = console1.Surface.Area.Center - (surfaceObject.Surface.Area.Size / 2);
+    surfaceObject.UseMouse = false;
+
+    console1.Children.Add(surfaceObject);
+
+    // Second console
+    Console console2 = new Console(58, 12);
+    console2.Position = new Point(19, 11);
+    console2.Surface.DefaultBackground = Color.AnsiRed;
+    console2.Clear();
+    console2.Print(1, 1, "Type on me!");
+    console2.Cursor.Position = new Point(1, 2);
+    console2.Cursor.IsEnabled = true;
+    console2.Cursor.IsVisible = true;
+    console2.FocusOnMouseClick = true;
+    console2.MoveToFrontOnMouseClick = true;
+
+    container.Children.MoveToBottom(console2);
+
+    container.Children.Add(console2);
+    container.Children.Add(console1);
 }
